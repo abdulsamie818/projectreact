309b0fd5e649aef7cc1600736a0e7bc9193b1e41
Abdul Samie <38508100+samamico@users.noreply.github.com>
2020-06-09 15:18:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 
 
 
 ReactDOM.render(
-  <div><App name="Abdul Samie" age={25} /></div>,
+  <div><App/></div>,
   
   document.getElementById('root')
 );
