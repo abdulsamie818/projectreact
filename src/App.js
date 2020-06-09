9fa2401cd530911711b25823399df423c5c55a91
Abdul Samie <38508100+samamico@users.noreply.github.com>
2020-06-09 13:57:17 +0200

    edited the code in  app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hello from "./Hello";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>
-          Edited by Abdul Samie
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+function App({name, age}) {
+  return <div>
+    <strong> Hello {name} & Age = {35-10} </strong>
+    <h1>This is H1 heading</h1> 
+    <h2>This is h2 heading</h2>
+    <ul type="disc">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
+
+<ol type="1">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ol>
+    <br/>
+    <Hello fName={name}></Hello>
     </div>
-  );
 }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hello.css'
+
+
+
+
+function Hello({fName}) {
+return <p className="myname">This is para in Hello Tag name : {fName}</p>
+}
+
+export default Hello;
